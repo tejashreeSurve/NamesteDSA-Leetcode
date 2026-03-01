bc9e341590694658fc9a3b402eeabea83ab6d64b
Tejashree Surve <tejashree_surve@epam.com>
2026-03-01 23:48:21 +0530

    45 Days - Leetcode

---
+++ LeetcodeProblems/BinaryTree/depthFirst&BreathFirstSearch.js
@@ -0,0 +1,20 @@
+/* 
+
+            A
+          /   \
+        B       c
+       / \       \
+      D   E       F
+
+DFS
+1. Used stack 
+2. travel down to the depth of the tree
+3. Pre-order =  A - B - D - F - C - F
+4. here we are travelling to the depth of the tree
+
+BFS 
+1. Used Queue 
+2. travel level by level
+3. A -  B - C - D - E - F
+
+*/
