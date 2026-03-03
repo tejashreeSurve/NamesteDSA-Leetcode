b62bcca5fe5e4472faf417f0051e73beaa20b9e0
Tejashree Surve <tejashree_surve@epam.com>
2026-03-03 22:53:34 +0530

    47 Days - Leetcode

--- LeetcodeProblems/BinaryTree/binaryTreeLevelOrder102.js
+++ LeetcodeProblems/BinaryTree/binaryTreeLevelOrder102.js
@@ -73,3 +73,17 @@ function iteravtive(root) {
     }
     return ans;
 }
+
+function recursive(root) {
+    if (!root) return []; 
+    let ans = [];
+    let traversal = (curr, level) => {
+        if (!ans[level]) ans[level] = [];
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    }
+
+    traversal(root, 0);
+    return ans;
+}
