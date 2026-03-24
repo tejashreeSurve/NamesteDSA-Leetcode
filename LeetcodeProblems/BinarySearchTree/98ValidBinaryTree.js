5049c66d4beb3392d4ccba7c16b9531635af7cea
Tejashree Surve <tejashree_surve@epam.com>
2026-03-25 00:06:18 +0530

    65 Days - Leetcode

---
+++ LeetcodeProblems/BinarySearchTree/98ValidBinaryTree.js
@@ -0,0 +1,58 @@
+/**
+ * 
+98. Validate Binary Search Tree
+
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys strictly less than the node's key.
+The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ */
+
+// right answer
+var isValidBST = function(curr, low,high) {
+        if (!curr) return true;
+
+        if ((low !== null && low >= curr.val) || (high !==null && high <= curr.val)) {
+            return false;
+        }
+
+        let leftSub = isValidBST(curr.left, low, curr.val);
+        let rightSub = isValidBST(curr.right, curr.val, high);
+
+        return leftSub && rightSub;
+};
+
+
+///this is what I tried
+var isValidBST = function(root) {
+    let ans = true;
+    const traversal =(curr)=>{
+        if(!curr.left || !curr.right) return;
+        if( !(curr.val >=curr.left.val) || !(curr.val <=curr.right.val)){
+            ans = ans && false;
+        }
+
+        curr.left && traversal(curr.left);
+        curr.right && traversal(curr.right);
+    }
+
+    traversal(root);
+
+    return ans
+};
---
+++ LeetcodeProblems/BinarySearchTree/binarySearchTreeIntro.js
@@ -0,0 +1,10 @@
+/**
+ * BST is a special typeof binary tree with atmost 2 children 
+ * For every node :
+ *  all node of left subtree have smaller value
+ *  all node of right subtree have greater value
+ * 
+ * searching element in  binary tree is very very fast  with time complexcity- O(log n)
+ * inroder traversal of BTS is always sorted
+ * 
+ */
