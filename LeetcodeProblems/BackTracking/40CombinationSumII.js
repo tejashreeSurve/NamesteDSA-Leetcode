e93e74d83d69d859f84503767f537a6c7bb1d54f
Tejashree Surve <tejashree_surve@epam.com>
2026-05-13 18:44:35 +0530

    81 days- Leetcode

---
+++ LeetcodeProblems/BackTracking/40CombinationSumII.js
@@ -0,0 +1,50 @@
+/**
+ * 
+40. Combination Sum II
+
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ */
+
+var combinationSum2 = function(arr, target) {
+    const result = [];
+    arr.sort((a,b)=> a - b);
+    const backTracking = (reminingSum, path, start)=>{
+        if(reminingSum === 0) result.push( [...path]);
+        if(reminingSum <=0) return;
+        for(let i = start;i<arr.length;i++){
+
+            if(i > start && arr[i] === arr[i-1]) continue;
+            path.push(arr[i]);
+            backTracking(reminingSum - arr[i], path, i+1);
+            path.pop();
+        }
+    }
+
+    backTracking(target, [], 0);
+
+
+    return result;
+};
